4a0652ae4c61cddff928da9fbb23c0bbfbcf6116
Andrew Goolsbee <andrew@andrewgoolsbee.com>
2025-11-09 13:39:56 -0600

    Add columns

--- transform/models/mart/legislators_current.sql
+++ transform/models/mart/legislators_current.sql
@@ -3,30 +3,48 @@
 }}
 
 with
-legislator_term_counts as (
+legislators_current as (
     select * from {{ ref('int_legislators_current') }}
 )
 
 select
-    trm.official_full_name,
-    trm.display_name,
-    trm.first_name,
-    trm.middle_name,
-    trm.last_name,
-    trm.name_suffix,
-    trm.nickname,
-    trm.state,
-    trm.type,
-    trm.party,
-    trm.term_count,
-    trm.first_term_start,
-    trm.last_term_end,
-    trm.gender,
-    trm.birthday,
-    trm.age,
-    trm.age_first_term_start,
-    trm.age_last_term_end,
-    trm.years_served,
-    trm.years_elected,
-    trm.years_remaining
-from legislator_term_counts as trm
+    leg.dlt_id,
+    leg.official_full_name,
+    leg.display_name,
+    leg.first_name,
+    leg.middle_name,
+    leg.last_name,
+    leg.name_suffix,
+    leg.nickname,
+    leg.state_code,
+    leg.state_name,
+    leg.chamber_code,
+    leg.chamber_name,
+    leg.party,
+    leg.term_count,
+    leg.first_term_start,
+    leg.last_term_end,
+    leg.gender,
+    leg.birthday,
+    leg.age,
+    leg.age_first_term_start,
+    leg.age_last_term_end,
+    leg.years_served,
+    leg.years_elected,
+    leg.years_remaining,
+    leg.ballotpedia_id,
+    leg.bioguide_id,
+    leg.cspan_id,
+    leg.google_entity_id,
+    leg.govtrack_id,
+    leg.house_history_id,
+    leg.icpsr_id,
+    leg.lis_id,
+    leg.maplight_id,
+    leg.opensecrets_id,
+    leg.pictorial_id,
+    leg.thomas_id,
+    leg.votesmart_id,
+    leg.wikidata_id,
+    leg.wikipedia_id
+from legislators_current as leg
