398c544683c7113f07b9b353bed1000051a93c7a
Andrew Goolsbee <andrew@andrewgoolsbee.com>
2025-11-09 13:37:51 -0600

    Add page config

--- app/main.py
+++ app/main.py
@@ -1,22 +1,24 @@
 import pandas as pd
 import streamlit as st
-
 from components import button_clear_cache
 
-@st.cache_data
-def get_house_data():
-    df = pd.read_parquet("load/house_legislators_current.parquet")
-    return df
-
-@st.cache_data
-def get_senate_data():
-    df = pd.read_parquet("load/senate_legislators_current.parquet")
-    return df
-
 def main():
+    st.set_page_config(
+        page_title="Congress Track",
+        page_icon=":material/account_balance:",
+        layout="wide",
+        initial_sidebar_state="auto",
+        menu_items={
+            # 'Get Help': 'https://',
+            # 'Report a bug': "https://",
+            'About': "# Congress Track\n Answer questions about U.S. Congress members."
+        }
+    )
+    
     button_clear_cache.button_clear_cache()
 
     st.title("Congress Track")
+
 
 if __name__ == "__main__":
     main()
