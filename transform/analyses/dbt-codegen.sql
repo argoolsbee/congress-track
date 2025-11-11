cdb46ce615ae85f7c95a31158640683bfc87a9c0
Andrew Goolsbee <andrew@andrewgoolsbee.com>
2025-11-11 01:33:53 -0600

    Add dbt-codegen

--- transform/analyses/dbt-codegen.sql
+++ transform/analyses/dbt-codegen.sql
@@ -1,18 +1,20 @@
 -- source
-
--- {{ codegen.generate_source(database_name= 'congress_legislators', schema_name= 'legislators_current') }}
-
+{#
+{{ codegen.generate_source(database_name= 'congress_track', schema_name= 'congress_legislators') }}
+#}
 
 -- model sql
-
--- {{ codegen.generate_base_model(
---     source_name='congress_legislators',
---     table_name='legislators_current'
--- ) }}
+{#
+{{ codegen.generate_base_model(
+    source_name='congress_legislators',
+    table_name='legislators_historical__terms__party_affiliations'
+) }}
+#}
 
 
 -- model yml
-
--- {{ codegen.generate_model_yaml(
---     model_names=['customers']
--- ) }}
+{#
+{{ codegen.generate_model_yaml(
+    model_names=['customers']
+) }}
+#}
